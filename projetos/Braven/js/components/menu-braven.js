ebb0070c33e288e515dc700fc52ddcf5da7d24ba
ricardo-fo <ricardo-cadastros@hotmail.com>
2019-01-28 20:32:07 -0200

    Braven + portfolio

---
+++ projetos/Braven/js/components/menu-braven.js
@@ -0,0 +1,27 @@
+// IIFE
+/* Funcionalidade: fazer o menu animado
+** Passo a passo:
+ * 1. Pegar o menu hamburguer; (OK)
+ * 2. Adicionar um evento de click no menu; (OK)
+ * 3. Pegar o items; (OK)
+ * 4. Adicionar a class -active no items quando o menu for clicado;
+*/
+
+"use strict";
+(function () {
+    const $menuBraven = document.querySelector(".menu-braven");
+    const $menu = $menuBraven.querySelector(".menu");
+    const $items = $menuBraven.querySelector(".items");
+
+    $menu.addEventListener("click", event => {
+        event.preventDefault();
+
+        $items.classList.add("-active");
+    });
+
+    $items.addEventListener("click", event => {
+        event.preventDefault();
+
+        $items.classList.remove("-active");
+    });
+})();
