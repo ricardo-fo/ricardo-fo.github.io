56f7b8cc5a6cf540e981f956d37bc80ba2f55b46
ricardo-fo <ricardo-cadastros@hotmail.com>
2019-02-03 17:07:21 -0200

    Backup

---
+++ js/hidePreloader.js
@@ -0,0 +1,6 @@
+function myFunction() {
+	var element = document.getElementById("preloader");
+	element.classList.remove("container-preloader");
+	element.classList.remove("caixa-preloader");
+	element.classList.remove("row-preloader");
+}
