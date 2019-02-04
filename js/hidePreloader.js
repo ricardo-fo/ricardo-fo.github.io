9a0454b5c22a5e27ec84f364a015cbd19c53b8b0
ricardo-fo <ricardo-cadastros@hotmail.com>
2019-02-04 17:45:18 -0200

    Portfolio-home

--- js/hidePreloader.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-	var element = document.getElementById("preloader");
-	element.classList.remove("container-preloader");
-	element.classList.remove("caixa-preloader");
-	element.classList.remove("row-preloader");
-}
