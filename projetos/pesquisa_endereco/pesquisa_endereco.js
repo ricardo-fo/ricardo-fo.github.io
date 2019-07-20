90ac78d6153c642fbbcbf34121da681fb9c5df18
Ricardo Freitas <ricardo-cadastros@hotmail.com>
2019-07-20 19:41:46 -0300

    App Pesquisa Endereco finalizado

---
+++ projetos/pesquisa_endereco/pesquisa_endereco.js
@@ -0,0 +1,20 @@
+function getDadosEnderecoPorCEP(cep){
+	let url = 'https://viacep.com.br/ws/' + cep + '/json/unicode/';
+	let xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.open('GET', url);
+
+	xmlHttp.onreadystatechange = () => {
+		if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+			let dadosJSONText = xmlHttp.responseText;
+			let dadosJSONObj = JSON.parse(dadosJSONText);
+			console.log(dadosJSONObj);
+			
+			document.getElementById('endereco').value = dadosJSONObj.logradouro;
+			document.getElementById('cidade').value = dadosJSONObj.localidade;
+			document.getElementById('bairro').value = dadosJSONObj.bairro;
+			document.getElementById('uf').value = dadosJSONObj.uf;
+		}
+	}
+	xmlHttp.send();
+}
