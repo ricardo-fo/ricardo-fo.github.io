19679f48b3f98918077224ac9a2188753e1a7296
Ricardo Freitas <ricardo-cadastros@hotmail.com>
2019-07-20 19:55:54 -0300

    Organizando arquivos de pesquisa_endereco

--- projetos/pesquisa_endereco/src/pesquisa_endereco.js
+++ projetos/pesquisa_endereco/src/pesquisa_endereco.js
@@ -1,15 +1,22 @@
 function getDadosEnderecoPorCEP(cep){
+	// Criação da url baseada no cep passado.
 	let url = 'https://viacep.com.br/ws/' + cep + '/json/unicode/';
+	// Instância da classe que controla as requisições XML.
 	let xmlHttp = new XMLHttpRequest();
-
 	xmlHttp.open('GET', url);
 
+	// Controle dos retornos dos estados informados pelo servidor.
 	xmlHttp.onreadystatechange = () => {
+		// Caso o estado tenha sido bem-sucedido.
 		if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+			// Vinculação da string contendo o JSON a uma variável.
 			let dadosJSONText = xmlHttp.responseText;
+
+			// Parse da string para a linguagem JSON.
 			let dadosJSONObj = JSON.parse(dadosJSONText);
-			console.log(dadosJSONObj);
+			//console.log(dadosJSONObj);
 			
+			// Modificando o DOM para exibir os dados.
 			document.getElementById('endereco').value = dadosJSONObj.logradouro;
 			document.getElementById('cidade').value = dadosJSONObj.localidade;
 			document.getElementById('bairro').value = dadosJSONObj.bairro;
